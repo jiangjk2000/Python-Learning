d3465ee5c2bd452215da2db6fcd085d1adddb99d
jiangjk2000 <jiangjk2000@outlook.com>
2019-07-12 17:37:57 +0800

    Update Car.py

--- Car.py
+++ Car.py
@@ -24,6 +24,31 @@ class Car():
         else:
             print("You can't roll back an odometer!")
 
+class Battery():
+    def __init__(self, battery_size=70):
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+    def get_range(self):
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+
+        message = "This car can go approximately " + str(range)
+        message += " miles on a full charge."
+        print(message)
+
+class ElectricCar(Car):
+    def __init__(self,make,model,year):
+        super().__init__(make,model,year)
+        self.battery = Battery()
+
+
+
+
 my_new_car = Car('audi','a4',2016)
 print(my_new_car.get_descriptive_name())
 my_new_car.update_odometer(44)
@@ -33,3 +58,8 @@ my_new_car.increment_odometer(100)
 my_new_car.increment_odometer(-100)
 #my_new_car.odometer_reading = 23
 my_new_car.read_odometer()
+
+my_tesla = ElectricCar('tesla','model s',2016)
+print(my_tesla.get_descriptive_name())
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
